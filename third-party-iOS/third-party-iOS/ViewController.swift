131ef8a2df965bdda6b2452123f30a11f034a87d
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-03-25 20:52:24 +0900

    add apple login function

--- third-party-iOS/third-party-iOS/ViewController.swift
+++ third-party-iOS/third-party-iOS/ViewController.swift
@@ -1,19 +1,58 @@
-//
-//  ViewController.swift
-//  third-party-iOS
-//
-//  Created by 神代泰宏 on 2023/03/25.
-//
-
+import AuthenticationServices
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var appleLoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
+extension ViewController {
+    @IBAction func appleLoginButtonPushed(_ sender: Any) {
+        loginApple()
+    }
+}
+
+extension ViewController {
+    // shiuld set on Sign in with Apple in Signing and Capabilities
+    private func loginApple() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        
+        controller.performRequests()
+    }
+}
+
+extension ViewController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+
+    func authorizationController(
+        controller: ASAuthorizationController,
+        didCompleteWithAuthorization authorization: ASAuthorization
+    ) {
+        guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential,
+              let authorizationCodeData = credential.authorizationCode,
+              let authorizationCode = String(data: authorizationCodeData, encoding: .utf8) else {
+                  print("don't get authorizationCode")
+                  return
+        }
+        // use authorizationCode to login
+        print(authorizationCode)
+    }
+    
+    func authorizationController(
+        controller: ASAuthorizationController,
+        didCompleteWithError error: Error
+    ) {
+        print("ERROR")
+        print(error)
+    }
+}
